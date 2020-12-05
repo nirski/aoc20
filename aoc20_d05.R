69dd7f3a60e46d65cf399edcd00bf6a3fa5482a8
Jarosław Nirski <nirski@yahoo.com>
2020-12-05 13:56:08 +0100

    Day 5 - corrected

--- aoc20_d05.R
+++ aoc20_d05.R
@@ -11,11 +11,10 @@ d05_r1 <- d05_d1 %>%
     mutate(bin = input %>% str_replace_all(c("F" = "0", "B" = "1", "L" = "0", "R" = "1"))) %>%
     # separate(bin, c("row", "col"), 7) %>%
     # mutate(row = strtoi(row, 2), col = strtoi(col, 2), seat = row * 8 + col) %>%
-    mutate(seat = bin %>% strtoi(2)) %>%
-    arrange(seat)
+    mutate(seat = bin %>% strtoi(2))
 
 d05_r1 %>% slice_max(seat)
 
 # Part 2 -------------------------------------------------------------------------------------------
 
-d05_r1 %>% slice_max(seat - lag(seat))
+d05_r1 %>% arrange(seat) %>% slice_max(seat - lag(seat))
