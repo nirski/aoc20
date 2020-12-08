6b4db0b4e38b62df814be70758e208106d4b6908
Jarosław Nirski <nirski@yahoo.com>
2020-12-08 08:49:30 +0100

    There is an .init in accumulate!

--- aoc20_d07.R
+++ aoc20_d07.R
@@ -11,29 +11,25 @@ d07_d1 <- read_advent_of_code(day = 7, cookie = read_lines("AOC_COOKIE"))
 d07_d2 <- enframe(d07_d1)
 
 d07_r1 <- d07_d2 %>%
-    extract(value, c("outside", "inside"), "^(.*) contain (.*)$") %>%
+    extract(value, c("outside", "inside"), "^(.*) bags contain (.*)$") %>%
     separate_rows(inside, sep = ", ") %>%
-    extract(outside, c("outside"), "^(.*) bags$") %>%
-    extract(inside, c("n", "inside"), "^([0-9]+) (.*) bags*\\.*$") %>%
+    extract(inside, c("n", "inside"), "^([0-9]+) (.*) bags*\\.*$", convert = TRUE) %>%
     mutate(
-        n = as.integer(n),
-        level = ifelse(inside == "shiny gold", 1, NA),
-        level2 = ifelse(outside == "shiny gold", 1, NA),
-        n2 = ifelse(outside == "shiny gold", n, NA)
+        level1 = ifelse(inside == "shiny gold", 1, NA),
+        n2 = ifelse(outside == "shiny gold", n, NA),
+        level2 = ifelse(outside == "shiny gold", 1, NA)
     ) %>%
     filter(!is.na(inside))
 
 # Part 1 -------------------------------------------------------------------------------------------
 
 d07_f1 <- function(df, lev) {
-    out <- df %>% filter(level == lev) %>% pull(outside)
-    df %>% mutate(level = ifelse(inside %in% out, lev + 1, level))
+    out <- df %>% filter(level1 == lev) %>% pull(outside)
+    df %>% mutate(level1 = ifelse(inside %in% out, lev + 1, level1))
 }
 
-d07_r1 %>%
-    list(1, 2, 3, 4, 5, 6, 7, 8, 9) %>%
-    accumulate(d07_f1) %>%
-    map(. %>% filter(!is.na(level)) %>% distinct(outside) %>% count()) %>%
+accumulate(1:9, d07_f1, .init = d07_r1) %>%
+    map(. %>% filter(!is.na(level1)) %>% distinct(outside) %>% count()) %>%
     unlist()
 
 # Part 2 -------------------------------------------------------------------------------------------
@@ -50,8 +46,6 @@ d07_f2 <- function(df, lev) {
         select(-n2.x, -n2.y)
 }
 
-d07_r1 %>%
-    list(1, 2, 3, 4, 5, 6, 7, 8, 9) %>%
-    accumulate(d07_f2) %>%
+accumulate(1:9, d07_f2, .init = d07_r1) %>%
     map(. %>% filter(!is.na(level2)) %>% tally(n2)) %>%
     unlist()
